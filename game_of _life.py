3f8b52dadfea71ab9b0c903c803646a6d76ec362
topdeliremegagroove <corentin.lucas@psl.eu>
2020-12-17 19:41:14 +0100

    main

--- game_of _life.py
+++ game_of _life.py
@@ -1,3 +1,77 @@
-print("Hello world")
+class plateau():
 
-print("goodbye world")
+    def __init__(self, fichier = None):
+        file = open("fichier.txt", "r")
+        plateau_racine = []
+        line = file.readline()
+        for line in file :
+            lignes[1] =
+        mon_fichier.close()
+
+
+
+    
+#fichier doit contenir la taille et l'état de chaque cel
+
+    def lignes(self, nbre_de_lignes)
+
+
+
+
+
+
+class case() :
+
+    def __init__(self, etat, cord)
+
+  
+    def coordonnees(self, x, y)
+        self.x = x
+        self.y = y
+
+    def etat(self, vivant = 0) :
+        return vivant 
+    #voir methode magique
+
+
+#main
+
+#première évaluation
+etat_voisin = 0
+for i in Lignes :
+    for j in Colonnes :
+        dans le plateau racine
+        etat_voisin = [i-1][j-1] + [i-1][j] + [i-1][j+1] + [i][j-1] + [i][j+1] + [i+1][j-1] + [i+1][j] + [i+1][j+1]
+        if (etat[i][j] == 0) and (etat_voisin == 3) :
+            etat[i][j] = 1 pour le nouveau plateau
+            rajouter +1 aux autres pour le tour d'après dans le plateau 1 au 8 fdp
+
+        elif  (etat[i][j] == 1) and ((etat_voisin < 2) or (etat_voisin >= 4)) :
+            etat[i][j] = 0
+        e
+        else : 
+
+        case.etat = 0
+        elif case.v
+
+#evaluation suivante n>2
+
+for i in Lignes :
+    for j in Colonnes :
+        if case.voisin = 
+
+
+
+
+
+
+    
+
+    
+
+
+
+
+
+
+
